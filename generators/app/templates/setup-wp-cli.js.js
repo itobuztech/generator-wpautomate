2030e7c656eb2ddabed9d6b5a71727a52919821e
Prosenijt Manna <developer.prosenjit@gmail.com>
2016-07-23 20:01:33 +0530

    wp cli installation task adding

--- generators/app/templates/setup-wp-cli.js.js
+++ generators/app/templates/setup-wp-cli.js.js
@@ -6,13 +6,16 @@ var g = require('gulp-load-plugins')();
 var config = require('./config.json');
 var p = require('../package.json');
 var exec = require('child_process').exec;
+var shell = require('shelljs');
 
 gulp.task('wpcli:install-core', function(){
-	exec('wp core download',function (err, stdout, stderr) {
-    if (stderr) {
-    	console.log(stderr)
-    }else {
-    	console.log('Wordpress installed.');
-    }
-  });
+	if(shell.exec('wp core download')) {
+		  shell.exec('wp core install --url="http://localhost/wpautomate" --title="Wpautomate" --admin_user="prosenjit" --admin_password="pass" --admin_email="prosenjit@itobuz.com" --skip-email');
+		  shell.exec('wp theme activate wpautomate');
+		  shell.exec('wp plugin delete hello');
+			shell.exec('wp plugin delete akismet');
+			shell.exec('wp theme delete twentyfifteen');
+			shell.exec('wp theme delete twentyfourteen');
+			shell.exec('wp theme delete twentysixteen');
+	}
 });

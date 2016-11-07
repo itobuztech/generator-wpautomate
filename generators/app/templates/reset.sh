70e6bb7037b607cdf1f09bb3926ea36f1a164cc0
Prosenjit Manna <developer.prosenjit@gmail.com>
2016-11-07 23:29:40 +0530

    reset script fixed

--- generators/app/templates/reset.sh
+++ generators/app/templates/reset.sh
@@ -4,7 +4,4 @@ wp db drop --yes
 wp db create
 wp core install --url="<%=siteUrl%>" --title="<%=projectName%>" --admin_user="<%=adminUser%>" --admin_password="<%=adminPass%>" --admin_email="<%=authorEmail%>"
 wp theme activate <%=projectName%>
-<% if (testData)  { %>
-#Install demo data
 wp import ./sh/wptest.xml --authors=create
-<% } %>
